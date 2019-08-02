3721374165e7bccfcad870132ec2efc72d63364f
ouyangbiao <biao.ouyang@myt11.com>
2019-08-02 14:53:00 +0800

    update .gitignore

--- Example/Pods/Target Support Files/OYAttributeString/OYAttributeString-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_OYAttributeString : NSObject
-@end
-@implementation PodsDummy_OYAttributeString
-@end
--- Example/Pods/Target Support Files/Pods-OYAttributeString_Tests/Pods-OYAttributeString_Tests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_OYAttributeString_Tests : NSObject
-@end
-@implementation PodsDummy_Pods_OYAttributeString_Tests
-@end
