7ae476d282c969df057d1f8ec1310fdd4944d913
ouyangbiao <biao.ouyang@myt11.com>
2019-08-02 15:04:00 +0800

    add image category

---
+++ OYAttributeString/UIImage+OYAttributeString.h
@@ -0,0 +1,22 @@
+//
+//  UIImage+OYAttributeString.h
+//  OYAttributeString
+//
+//  Created by t11 on 2019/8/2.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface UIImage (OYAttributeString)
+
++ (instancetype)imageWithColor:(UIColor *)color size:(CGSize)size;
+
+- (NSAttributedString *)attributedString;
+
+- (NSTextAttachment *)attachment;
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ OYAttributeString/UIImage+OYAttributeString.m
@@ -0,0 +1,34 @@
+//
+//  UIImage+OYAttributeString.m
+//  OYAttributeString
+//
+//  Created by t11 on 2019/8/2.
+//
+
+#import "UIImage+OYAttributeString.h"
+
+@implementation UIImage (OYAttributeString)
+
++ (instancetype)imageWithColor:(UIColor *)color size:(CGSize)size {
+    UIGraphicsBeginImageContextWithOptions(size, NO, 0.0);
+    CGContextRef context = UIGraphicsGetCurrentContext();
+    CGContextSetFillColorWithColor(context, color.CGColor);
+    CGContextFillRect(context, CGRectMake(0.0, 0.0, size.width, size.height));
+    UIImage *image = UIGraphicsGetImageFromCurrentImageContext();
+    UIGraphicsEndImageContext();
+    return image;
+}
+
+- (NSAttributedString *)attributedString {
+    NSTextAttachment *attachment = self.attachment;
+    return [NSAttributedString attributedStringWithAttachment:attachment].copy;
+}
+
+- (NSTextAttachment *)attachment {
+    NSTextAttachment *attachment = [[NSTextAttachment alloc] init];
+    attachment.image = self;
+    attachment.bounds = CGRectMake(0.0, 0.0, self.size.width, self.size.height);
+    return attachment;
+}
+
+@end
